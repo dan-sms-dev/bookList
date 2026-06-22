e8a05d87369595e096d50cfe09276abc2854d86a
Daniel <danielsoares.ms07@gmail.com>
2026-06-22 18:54:15 -0300

    Add books index route and create books index view

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('index');
+
+Route::get('/books', function () {
+    return view('books.index');
+})->name('books.index');
